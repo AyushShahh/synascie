e92b1da76fef992e5f8afdca2d1eccfc24df9187
Ayush Shah <shahayush001@gmail.com>
2023-03-12 03:21:05 +0530

    now using dom-to-image to capture jpeg snapshots

--- static/image.js
+++ static/image.js
@@ -13,15 +13,22 @@
 //     })
 // })
 
-$(function() { 
-    $("#save").click(function() { 
-        html2canvas($("#art"), {
-            onrendered: function(canvas) {
-                theCanvas = canvas;
-                canvas.toBlob(function(blob) {
-                    saveAs(blob, "ascii.png");
-                });
-            }
+var node = document.getElementById('art');
+var scale = 4;
+
+document.getElementById("save").addEventListener('click', () => {
+    domtoimage.toJpeg(art, {
+        width: art.clientWidth * scale,
+        height: art.clientHeight * scale,
+        quality: 1,
+        style: {
+            transform: 'scale('+scale+')',
+            transformOrigin: 'top left'
+        }})
+        .then(function (dataUrl) {
+            var link = document.createElement('a');
+            link.download = 'ascii.jpeg';
+            link.href = dataUrl;
+            link.click();
         });
-    });
-});  
+})
