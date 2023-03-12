6b0086414e77eb31dfe8f75a352f1d95fa290194
Ayush Shah <shahayush001@gmail.com>
2023-03-12 19:53:34 +0530

    remove support for instagram's in-app browser

--- static/index.js
+++ static/index.js
@@ -1,17 +1,33 @@
-// Get user's device screen width and height
-document.getElementById('browser-resolution').value = screen.width + "x" + screen.height;
+if(navigator.userAgent.includes("Instagram"))
+{
+  document.querySelector('.upload').innerHTML = "<h2 style='text-decoration:line-through black 3px'>Upload an image</h2><p class='orange' style='font-size:smaller; word-break: normal'>You are currently using Instagram's in-app browser</p><p style='word-break: normal'>Instagram's in-app browser has a lot of limitations and lesser features so this site does not work on on this browser.<br><br>Kindly open this site in your mobile's native browser (chrome/safari/etc) by clicking on the three dots &nbsp;<b style='font-size:larger'>&#8942;</b>&nbsp; in the top right corner or,<br><br>click on <b id='txt' style='color:lightskyblue; text-decoration:underline'>https://asciiiart.vercel.app/</b> this link to copy and then head over to your mobile's native browser and paste it to open this site.</p><p class='orange' id='copy' style='font-size:smaller; text-align:center'></p>";
 
-// Get size of image and set its value
-function getAndPostSize(event) {
-  const file = event.target.files[0];
-  if (file) {
-    const fileSize = file.size; // in bytes
-    document.getElementById('size').value = fileSize;
-    // If greater than 4.5 MB, remove image file and submit form
-    if (fileSize > 4.5 * 1024 * 1024) 
-    {
-      document.getElementById("file").remove();
-      document.getElementById("form").submit();
+  $("#txt").on("click", function (event) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($("#txt").html()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    $("#copy").html("Link copied!");
+  });
+}
+
+else {
+  // Get user's device screen width and height
+  document.getElementById('browser-resolution').value = screen.width + "x" + screen.height;
+
+  // Get size of image and set its value
+  function getAndPostSize(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const fileSize = file.size; // in bytes
+      document.getElementById('size').value = fileSize;
+      // If greater than 4.5 MB, remove image file and submit form
+      if (fileSize > 4.5 * 1024 * 1024) 
+      {
+        document.getElementById("file").remove();
+        document.getElementById("form").submit();
+      }
     }
   }
 }
