c4fe9ba5bde9c701318a879d2987ccecfad31643
Ayush Shah <shahayush001@gmail.com>
2023-03-18 17:51:52 +0530

    change background image before download

--- static/image.js
+++ static/image.js
@@ -25,8 +25,8 @@ button.addEventListener('click', () => {
         scale: 5, // set the scale factor to 5
         backgroundColor: null, // set background color to transparent
       }).then(function(canvas) {
-        location.reload();
         // convert canvas to data URL and download as JPEG
+        document.body.style.backgroundImage = "url(/static/background.jpg)";
         var link = document.createElement("a");
         link.download = "ascii.jpeg";
         link.href = canvas.toDataURL("image/jpeg", 1.0);
